5ea943fc08462e911d97e0405d372ae060b386ea
Claes Adamsson <claes.adamsson@ingka.ikea.com>
2026-01-31 12:14:32 +0100

    feat(core): initial project setup with library structure

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+/// Main entry point for medi
+/// The application logic is contained in lib.rs, and this file is a thin wrapper responsible
+/// only for parsing arguments and handling top-level errors.
+fn main() {
+    println!("Hello, world!")
+}
